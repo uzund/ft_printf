42dc6ef7bf9e981ecb8b4f4a69ebf8c4864c383e
Davut Uzun <duzun@k1m35s10.42istanbul.com.tr>
2022-08-25 22:37:48 +0300

    ft_printf devam ...

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: duzun <davut@uzun.ist>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/24 06:49:19 by duzun             #+#    #+#             */
-/*   Updated: 2022/08/25 22:11:07 by duzun            ###   ########.fr       */
+/*   Updated: 2022/08/25 22:35:24 by duzun            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,4 +43,10 @@ int	ft_printf(const char *str, ...)
 	}
 	va_end(arg);
 	return (val);
+}
+
+int	main(void)
+{
+	ft_printf("d");
+	return (0);
 }
